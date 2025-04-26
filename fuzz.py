1edf4d2450808fcb5c2ccac5947c29fec16bece1
terryedmondsonjr <trey.edmondson22@gmail.com>
2025-04-26 12:20:12 -0500

    Five Fuzzing Methods with Comments and Printing

--- fuzz.py
+++ fuzz.py
@@ -3,7 +3,7 @@ import scanner
 
 def fuzzer():
 
-    #Fuzzing graphtaint's constructHelmString function with a tuple not of size 3
+    print("Fuzzing graphtaint's constructHelmString function with a tuple not of size 3")
     #upper_key, key, _ = hiera_tuple does not verify integrity of the tuple before attempting to assign it
     try:
         print(graphtaint.constructHelmString(('only', 'two')))
@@ -17,6 +17,7 @@ def fuzzer():
 
     #Fuzzing scanner's runScanner function with an invalid folder directory
     #Tries to return empty value sarif_json which errors out
+    print("Fuzzing scanner's runScanner function with an invalid folder directory")
     try:
         content_as_ls, sarif_json = scanner.runScanner('/nonexistant/')
     except Exception as e:
@@ -24,6 +25,7 @@ def fuzzer():
 
     #Fuzzing graphtaint's getValidTaints function with a tuple of not size two
     #script_name, helm_string = match does not verify integrity of the tuple before assigning it
+    print("Fuzzing graphtaint's getValidTaints function with a tuple of not size two")
     try:
         script_name, helmstring = graphtaint.getValidTaints(("one", "too", "many"))
     except Exception as e:
@@ -33,4 +35,18 @@ def fuzzer():
     except Exception as e:
         print(f"Error: {e}")
 
-    #names = scanner.scanUserName('user', 'notalist')
+    #Fuzzing scanner's scanUserName function with a noniterable type None
+    #for val_ in val_lis: where val_list is not typechecked
+    print("Fuzzing scanner's scanUserName function with a noniterable type None")
+    try:
+        names = scanner.scanUserName('user', None)
+    except Exception as e:
+        print(f"Error: {e}")
+
+    #Fuzzing scanner's scanPasswords function with a noniterable type None
+    #for val_ in val_lis: where val_list is not typechecked
+    print("Fuzzing scanner's scanPasswords function with a noniterable type None")
+    try:
+        passwords = scanner.scanPasswords('passwd', None)
+    except Exception as e:
+        print(f"Error: {e}")
